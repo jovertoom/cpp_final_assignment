6ad4fc1628bb590036b256effaf9e23679f6e168
Jelle Overtoom <j.f.j.overtoom@student.tudelft.nl>
2017-05-15 18:46:10 +0200

    nieuwe mappen

---
+++ src/matrix.cxx
@@ -0,0 +1,56 @@
+//Methods
+using namespace std;
+
+template<typename T> Matrix<T>::Matrix(int h,int w) :rows(h), cols(w))//Constructor
+{
+    rows = h;
+    cols = w;
+    m = new T*[h];
+    for(int i = 0; i < h; i++)
+    {
+        m[i] = new T[w];
+    }
+}
+
+template<typename T> Matrix<T>& Matrix<T>::operator[](std::initializer_list<T> list)
+{
+    std::uninitialized_copy(list.begin(), list.end(), index);
+    return *this;
+};
+
+template<typename T> Matrix<T>& Matrix<T>::operator=(T value) //sets value of the index
+{
+    data[index[0]].v[index[1]]=value;
+    return *this;
+};
+
+
+/**
+template<typename T, typename S> // Matrix x Scalar
+Matrix<typename std::common_type<T,S>::type> operator*(const Matrix<T>& mat,const S s)
+{
+    Matrix<typename std::common_type<T,S>::type> result(mat.rows,mat.cols);
+    for(auto i = 0;i<mat.h;i++)
+    {
+        result[i]=mat[i]*s;
+    }
+    return result;
+};
+
+template<typename T, typename S> //Scalar x Matrix
+Matrix<typename std::common_type<T,S>::type> operator*(const S s,const Matrix<T>& mat)
+{
+    return mat*s;
+};**/
+
+
+template<typename T, typename M>    //Matrix x Vector
+Vector<typename std::common_type<T,M>::type> operator*(const Matrix<T>& mat,Vector<M> vec)
+{
+    Vector<typename std::common_type<T,M>::type> result(mat.rows);
+    for(auto i = 0;i<mat.h;i++)
+    {
+        result.v[i]=dot(mat[i],vec);
+    }
+    return result;
+};
---
+++ src/matrix.h
@@ -0,0 +1,31 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <memory>
+using namespace std;
+
+template<typename T> class Matrix{
+public :
+    std::vector<Vector<T>> data;
+    int index[2];
+    int rows, cols;
+    T** m;
+
+    
+    Matrix(int h,int w); 
+    Matrix<T>& operator[](std::initializer_list<T> list);
+    Matrix<T>& operator=(T value);
+};
+
+
+template<typename T, typename M>    //Matrix x Vector
+Matrix<typename std::common_type<T,M>::type> operator*(const Matrix<T>& mat,Vector<M> vec);
+
+template<typename T, typename S>
+Matrix<typename std::common_type<T,S>::type> operator*(const Matrix<T>& mat,const S s); 
+
+template<typename T, typename S>
+Matrix<typename std::common_type<T,S>::type> operator*(const S s,const Matrix<T>& mat); 
+
+#include "matrix.cxx"
+#endif 
